68109d81926df69136c42fb1bd7feae629cf0ad4
matphys <razilya5858@yandex.ru>
2015-11-17 11:37:39 +0300

    Update hero.py

--- hero.py
+++ hero.py
@@ -1,6 +1,16 @@
 # coding: utf-8
 # license: GPLv3
 from gameunit import *
+class Hero(Attacker):
+    def __init__(self,name):
+        self._health=100
+        self._attack=50
+        self._experience=0
+    def attack(self,target):
+        target._health -= self._attack
+        Hero._experience += 1
+    def gameOver(self):
+        return self._health <= 0 
 
 #FIXME:
 """В этом файле должен быть описан класс героя, унаследованный от Attacker
